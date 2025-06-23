54d6750b41479d526f6b137abd3a015b05482a32
Sathyan S S <sssathyan2004@gmail.com>
2025-06-23 20:31:09 +0530

    Update not_gate.v

--- not gate/not_gate.v
+++ not gate/not_gate.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 23.06.2025 08:43:31
-// Design Name: 
-// Module Name: not_gate
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module not_gate(
     input a,
