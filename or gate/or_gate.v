ce309f5519fcfa5b2641a9b61e6194b2c5717cbd
Sathyan S S <sssathyan2004@gmail.com>
2025-06-23 20:31:40 +0530

    Update or_gate.v

--- or gate/or_gate.v
+++ or gate/or_gate.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 21.06.2025 20:22:59
-// Design Name: 
-// Module Name: or_gate
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module or_gate(
     input a,
