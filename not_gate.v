ca1609ac9af875a4123c697db2d7cf6a6f0fea5c
Sathyan S S <sssathyan2004@gmail.com>
2025-06-23 19:54:41 +0530

    Add files via upload

---
+++ not_gate.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 23.06.2025 08:43:31
+// Design Name: 
+// Module Name: not_gate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module not_gate(
+    input a,
+    output y);
+    assign y = ~a;
+endmodule
