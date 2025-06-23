fb34bcb55a0f03baba91398e0960300e2558e45a
Sathyan S S <sssathyan2004@gmail.com>
2025-06-23 20:28:28 +0530

    Updated and gate verilog file

---
+++ and gate/and_gate_.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 21.06.2025 10:45:03
+// Design Name: 
+// Module Name: and_gate_
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module and_gate_(
+    input a,
+    input b,
+    output y
+);
+assign y=a&b;
+endmodule
