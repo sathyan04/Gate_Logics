f43f8817ca98fd343fb25761d7230bf408f1bfce
Sathyan S S <sssathyan2004@gmail.com>
2025-06-23 20:30:52 +0530

    Update and_gate_tb.v

--- and gate/and_gate_tb.v
+++ and gate/and_gate_tb.v
@@ -1,25 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 21.06.2025 10:47:29
-// Design Name: 
-// Module Name: and_gate_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
-`timescale 1ns / 1ps
 module and_gate_tb;
 
   reg a, b;
