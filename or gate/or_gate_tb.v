472369354ff9db6b0a3b1583b6106afd6cd4952f
Sathyan S S <sssathyan2004@gmail.com>
2025-06-23 20:32:00 +0530

    Update or_gate_tb.v

--- or gate/or_gate_tb.v
+++ or gate/or_gate_tb.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 21.06.2025 20:23:52
-// Design Name: 
-// Module Name: or_gate_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 module or_gate_tb;
 
